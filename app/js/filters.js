b1cc1fdcbe2c8f0fd11cbe28a9d0c4651f37bd2a
dcraven <dcravenus@gmail.com>
2013-01-18 11:44:11 -0700

    Added language filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,4 +2,21 @@
 
 /* Filters */
 
-angular.module('hummedia.filters', []);
+angular.module('hummedia.filters', [])
+.filter('language', function () {
+    return function(n){
+        switch(n)
+        {
+            case 'de':
+                return 'German';
+            case 'pt':
+                return 'Portuguese';
+            case 'en':
+                return 'English';
+            case 'zh':
+                return 'Chinese';
+            default:
+                return n;
+        }
+    };
+});
