f69146a8336a6e1c727787830a3aff76562e81c1
dcraven <dcravenus@gmail.com>
2013-01-18 13:42:27 -0700

    Users can toggle between list and thumbnail views

--- app/js/controllers/collection.js
+++ app/js/controllers/collection.js
@@ -2,9 +2,10 @@
  * Handles searching and displaying search results to the user
  */
 'use strict';
-function CollectionCtrl($scope, $routeParams, Collection) {
+function CollectionCtrl($scope, $routeParams, Collection, $location) {
     $scope.collection_data = Collection.get({identifier:$routeParams.id, full:'true'});
-    console.log($scope.collection_data);
+    //console.log($scope.collection_data);
+    $scope.path = $location.url();
 }
 // always inject this in so we can later compress this JavaScript
-CollectionCtrl.$inject = ['$scope', '$routeParams', 'Collection'];
+CollectionCtrl.$inject = ['$scope', '$routeParams', 'Collection', '$location'];
