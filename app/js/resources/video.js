6641fd93d0b1dd99f4ac5aaddbee4328047c3dc6
Ian Hunter <beanland2@gmail.com>
2013-01-23 19:38:43 -0700

    [Refs #42164761] Resource now enables advanced searching.

--- app/js/resources/video.js
+++ app/js/resources/video.js
@@ -1,10 +1,33 @@
 'use strict';
 
 angular.module('hummedia.services').
-    factory('Video', ['$resource', 'appConfig', function($resource, config){
+    factory('Video', ['$resource', 'appConfig', function($resource, config, $q){
         var resource = $resource(config.apiBase + '/video/:identifier', {identifier: '@identifier'},
         {
             search: {method: 'GET', isArray: true, params: {searchtype: 'keyword', q: '@q'}}
         });
+    
+	/**
+	 * Wrapper for the search method that alters the parameter list passed in.
+	 * This will change all parameter values into a single string passed through
+	 * the q parameter, as per the API's expectations
+	 * 
+	 * @params - same as $resource.get()'s parameters
+	 */
+	resource.advancedSearch = function(params){
+	    var newArgs = arguments;
+	    var advParams = {searchtype: 'advanced', q: ''};
+	    
+	    // note that these are auto URI-encoded by the resource.search method below
+	    angular.forEach(params, function(val, key) {
+		advParams.q += (advParams.q? "&" : "") + "dc." + key + ":" + val;
+	    });
+	    newArgs[0] = advParams;
+	    
+	    /**
+	     * @todo: No idea what "this" should be here, so I'm just using the current scope
+	     */
+	    return resource.search.apply(this, newArgs);
+	};
         return resource;
     }]);
