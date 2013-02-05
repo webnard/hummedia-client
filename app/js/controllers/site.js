9f231e3cc8c1303e02713a2b1e4e9107af854ed1
dcraven <dcravenus@gmail.com>
2013-02-05 13:13:48 -0700

    Changed flickr stuff to draw from array of ids

--- app/js/controllers/site.js
+++ app/js/controllers/site.js
@@ -4,35 +4,31 @@ function SiteCtrl($scope, $http) {
     function getRandomInt (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    $http.jsonp("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=78516abde0b618f9816d5efb10642812&tags=nature%2C+architecture&license=1&privacy_filter=1&safe_search=1&content_type=1&format=json&jsoncallback=JSON_CALLBACK")
+    var photo_ids = ['510175383','30950009', '2685376029', '5803011292', '7608871682'];
+    //var photo_ids = ['7608871682'];
+    var imagecount = photo_ids.length;
+    var num = getRandomInt(0,imagecount-1);
+    var photo_id = photo_ids[num];
+    console.log(photo_id);
+    $http.jsonp("http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=b87e337e9091368d25a03a475c8836b2&photo_id="+photo_id+"&format=json&jsoncallback=JSON_CALLBACK")
     .success(function(data){
-        var images = data.photos.photo;
-        var imagecount = images.length;
-        var num = getRandomInt(0,imagecount);
-        var farm_id = images[num]['farm'];
-        var server_id = images[num]['server'];
-        var id = images[num]['id'];
-        var secret = images[num]['secret'];
+        var photo = data.photo;
+        console.log(photo);
+        var farm_id = photo.farm;
+        var server_id = photo.server;
+        var id = photo.id;
+        var secret = photo.secret;
         $scope.img_url = "http://farm"+ farm_id + ".staticflickr.com/" + server_id + "/" + id +"_" + secret + "_b.jpg";
-        $scope.image = images[num];
-        console.log($scope.image);
-        //Get Photo Creator data
-        $http.jsonp("http://api.flickr.com/services/rest/?method=flickr.people.getInfo&api_key=78516abde0b618f9816d5efb10642812&user_id="+$scope.image.owner+"&format=json&jsoncallback=JSON_CALLBACK")
-        .success(function(data){
-            if(data.person.hasOwnProperty('realname') && data.person.realname._content!=''){
-                $scope.img_username = data.person.realname._content;
+        $scope.image_title = photo['title']['_content'];
+        $scope.img_profile = photo['urls']['url']['0']['_content'];
+        var owner = photo.owner;
+        if(owner.hasOwnProperty('realname') && owner.realname !==''){
+                $scope.img_username = owner.realname;
             }
             else{
-                $scope.img_username = data.person.username._content;
+                $scope.img_username = owner.username;
             };
-            $scope.img_userprofile = data.person.profileurl._content;
-            if(data.person.path_alias==null){
-                $scope.img_profile = "http://www.flickr.com/photos/"+data.person.id+"/"+id+"/";
-            }else{
-                $scope.img_profile = "http://www.flickr.com/photos/"+data.person.path_alias+"/"+id+"/";
-            }
-            console.log(data);
-        });
+            
     });
 }
 // always inject this in so we can later compress this JavaScript
