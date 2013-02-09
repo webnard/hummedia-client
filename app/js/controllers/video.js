049a452ff3f15ab09bdcbe000445cd0fc6b53746
dcraven <dcravenus@gmail.com>
2013-02-09 16:25:26 -0700

    More API fixes plus added flowplayer to show video

--- app/js/controllers/video.js
+++ app/js/controllers/video.js
@@ -1,8 +1,6 @@
 'use strict';
 function VideoCtrl($scope, $routeParams, Video) {
     $scope.video = Video.get({identifier:$routeParams.id});
-    console.log($scope.video);
-
 }
 // always inject this in so we can later compress this JavaScript
 VideoCtrl.$inject = ['$scope', '$routeParams', 'Video'];
