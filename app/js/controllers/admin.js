e4171ec6c0e7972a3d0660b0cbb1bc94023e6221
dcraven <dcravenus@gmail.com>
2013-02-18 12:19:59 -0700

    Ugly delete and add functions added

--- app/js/controllers/admin.js
+++ app/js/controllers/admin.js
@@ -1,8 +1,14 @@
 'use strict';
 function AdminCtrl($scope, $routeParams, Collection) {
     $scope.collections = Collection.query();
-   
-    setTimeout(function(){console.log($scope.collections)}, 1000);
+    $scope.newCollection = function(){
+        Collection.save({"dc:title":"New Collection"});
+        $scope.collections = Collection.query();
+    };
+    $scope.deleteCollection = function(pid){
+        Collection.delete({"identifier":pid});
+        $scope.collections = Collection.query();
+    };
 }
 // always inject this in so we can later compress this JavaScript
 AdminCtrl.$inject = ['$scope', '$routeParams', 'Collection'];
