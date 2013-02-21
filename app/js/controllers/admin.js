db232508eb7583384b1ac2faad0421b70f1b305d
dcraven <dcravenus@gmail.com>
2013-02-21 11:16:52 -0700

    Admin page is prettier

--- app/js/controllers/admin.js
+++ app/js/controllers/admin.js
@@ -2,12 +2,30 @@
 function AdminCtrl($scope, $routeParams, Collection) {
     $scope.collections = Collection.query();
     $scope.newCollection = function(){
-        Collection.save({"dc:title":"New Collection"});
+        Collection.save({
+            "dc:title":$scope.newtitle,
+            "dc:description":$scope.newdescription,
+            "dc:creator":$scope.newcreator
+        });
+        $scope.newtitle = '';
+        $scope.newdescription ='';
+        $scope.newcreator='';
         $scope.collections = Collection.query();
     };
     $scope.deleteCollection = function(pid){
         Collection.delete({"identifier":pid});
         $scope.collections = Collection.query();
+    };
+    $scope.delete2Collection = function(pid){
+        var entry = document.getElementById('collection-'+pid);
+        var delbutton = document.getElementById('deletebutton');
+        $(document).ready(function() {
+            $('li').css("background-color","white");
+            //$('li').addClass("listvideo");
+            entry.style.backgroundColor="lightblue";
+            delbutton.onclick = function(){alert(pid);};
+            delbutton.disabled = false;
+        });
     };
 }
 // always inject this in so we can later compress this JavaScript
