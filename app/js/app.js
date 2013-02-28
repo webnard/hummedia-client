ab6268ab136071376af2b33d8bf2fc0ef0a71874
Ian Hunter <beanland2@gmail.com>
2013-02-27 17:41:39 -0700

    Initial workings for HTTP Response interceptors for API errors.

--- app/js/app.js
+++ app/js/app.js
@@ -10,4 +10,36 @@ angular.module('hummedia', ['hummedia.config','hummedia.filters', 'hummedia.serv
   }]).
   config(['$locationProvider', function($locationProvider) {
     //$locationProvider.html5Mode(true);
+  }]).
+  config(['$httpProvider', function($httpProvider) {
+      
+      // Intercepts HTTP requests to display API-related errors to the user
+      // see http://docs.angularjs.org/api/ng.$http
+      $httpProvider.responseInterceptors.push(['$q', 'appConfig', function($q, appConfig){
+          return function(promise) {
+              return promise.then(function(response){
+                  // success, do nothing
+                  return response;
+              }, function(response) {
+                  // if this is a local request (i.e., not one to flickr or some other service)
+                  // then we go back to normal
+                  if(response.config.url.indexOf(appConfig.apiBase) !== 0) {
+                      return response;
+                  }
+                  console.log(response);
+                  switch(response.status) {
+                      case 404:
+                          console.log("Something's missing.");
+                          break;
+                      case 401:
+                      case 403:
+                          console.log("Log in or something.");
+                          break;
+                      default:
+                          console.log("Broken!!");
+                  }
+                  return $q.reject(response);
+              });
+          };
+      }]);
   }]);
