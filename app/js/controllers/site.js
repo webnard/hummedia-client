6d9199dd09e962e2b86b27b7411bfe17d04b2888
Ian Hunter <beanland2@gmail.com>
2013-03-05 10:35:19 -0700

    Fixing typo that caused a background-image flicker.

--- app/js/controllers/site.js
+++ app/js/controllers/site.js
@@ -45,7 +45,7 @@ function SiteCtrl($scope, $http, appConfig) {
             defaultImage();
             throw error;
         }
-    }).error(defaultImage());
+    }).error(defaultImage);
 
     
 }
