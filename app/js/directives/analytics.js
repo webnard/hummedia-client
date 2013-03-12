060def1eb05ef9d29a3ef0e080f5ef0b2cf37a1d
Ian Hunter <beanland2@gmail.com>
2013-03-12 14:48:53 -0600

    Analytics attributes now functional with page elements.

--- app/js/directives/analytics.js
+++ app/js/directives/analytics.js
@@ -6,7 +6,29 @@ HUMMEDIA_DIRECTIVES
                 replace: false,
                 compile: function(element, attrs, transclude) {
                     return function postLink(scope, iElement, iAttrs, controller) {
-                        console.log(iAttrs);
+                        
+                        var required_attrs = ['analyticsLabel','analyticsCategory'];
+                        
+                        angular.forEach(required_attrs, function(a) {
+                            if(!iAttrs[a]) {
+                                console.log(iAttrs);
+                                throw "Missing attribute or value for '" + a + "'";
+                            }
+                        });
+                        
+                        /**
+                         * @todo: Keep this in here?
+                         */
+                        if(!iAttrs.analytics) {
+                            console.warn("Analytics event added without events to ", element);
+                            return;
+                        }
+                        var events = iAttrs.analytics.split(/,\s*/);
+                        angular.forEach(events, function(event){
+                            element.bind(event, function(){
+                                analytics.event(iAttrs['analyticsCategory'],event,iAttrs['analyticsLabel']);
+                            });
+                        });
                     };
                 }
             };
