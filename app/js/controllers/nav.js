29719aedeffbaf712352fab0b81406baf8690487
Ian Hunter <beanland2@gmail.com>
2013-04-19 13:05:25 -0600

    Going for a more standard navigation-menu-based approach.

--- app/js/controllers/nav.js
+++ app/js/controllers/nav.js
@@ -1,5 +1,14 @@
 'use strict';
 function NavCtrl($scope, user) {
     $scope.user = user;
+    $scope.showAdminMenu = false;
+    $scope.showLogin = true;
+
+    $scope.$watch(function(){ return !user.exists && !user.netIDRequired }, function(show) {
+        $scope.showLogin = show;
+    });
+    $scope.toggleAdmin = function() {
+        $scope.showAdminMenu = !$scope.showAdminMenu;
+    };
 };
 NavCtrl.$inject = ['$scope','user'];
