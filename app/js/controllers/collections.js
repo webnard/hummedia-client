7037d7d61abf2018a01fdcdaaaea60bfa99dd6dc
dcraven <dcravenus@gmail.com>
2013-04-23 15:09:31 -0600

    Stop using so many ng-repeats, now just replacing things in the scope

--- app/js/controllers/collections.js
+++ app/js/controllers/collections.js
@@ -1,21 +1,22 @@
 'use strict';
 function CollectionsCtrl($scope, $routeParams, Collection) {
     $scope.collections = Collection.query();
-    
     $scope.$watch(function(){return $scope.collections.length;}, function(){
+        $scope.collection = $scope.collections[0];        
         $scope.collections_data=[];
+            $('#tabs p:first-child').addClass("highlighted");
         for(var i=0; i<$scope.collections.length; i++){
             var pid = $scope.collections[i]['pid'];
             var item = Collection.get({identifier:pid});
             $scope['collections_data'][pid] = item;
         }            
     });
-    
     $scope.showVideos = function(pid){
-        $('.collection_toggle').hide();
-        $('#'+pid).show();
-        $('#tabs p').removeClass("highlighted");
-        $('#tab-'+pid).addClass("highlighted");
+        for(var i=0; i<$scope.collections.length; i++){
+            if($scope.collections[i]['pid']===pid){
+                $scope.collection = $scope.collections[i];
+            }
+        }       
     };
 }
 // always inject this in so we can later compress this JavaScript
