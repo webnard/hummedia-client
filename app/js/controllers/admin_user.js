345424d005a2ec32c2c723972b4d56280b527d84
Ian Hunter <beanland2@gmail.com>
2013-04-23 12:47:04 -0600

    Administration for user nearly entirely in place. Can update/delete users, but role-setting still missing.

--- app/js/controllers/admin_user.js
+++ app/js/controllers/admin_user.js
@@ -1,10 +1,39 @@
-function AdminUserCtrl($scope, account) {
+function AdminUserCtrl($scope, account, language) {
     "use strict";
-    $scope.user = null;
+    
+    $scope.languages = language.list;
     $scope.users =  account.search();
+    $scope.user = null;
+    var copy = null; // used for updating the user list while removing $scope.user
 
     $scope.setUser = function(user) {
+        copy = user;
         $scope.user = user;
     };
+
+    $scope.cancel = function() {
+        $scope.user = null;
+        copy.$get().success(function(){copy = null});
+    }
+
+    // remember, delete is a reserved word, so we need to do this for compilation
+    $scope['delete'] = function(user) {
+        if(!confirm("Are you sure you want to delete " + user.fullname + "(ID: " + user._id + ")?")) {
+            return;
+        }
+        $scope.users.forEach(function(u, index) {
+            $scope.user = null;
+            copy = null;
+            if(user === u) {
+                user.$delete(function() {
+                    $scope.users.splice(index,1);
+                });
+            }
+        });
+    }
+
+    $scope.update = function() {
+        $scope.user.$update();
+    }
 };
-AdminUserCtrl.$inject = ['$scope','Account'];
+AdminUserCtrl.$inject = ['$scope','Account', 'language'];
--- app/js/resources/account.js
+++ app/js/resources/account.js
@@ -2,10 +2,10 @@
 
 angular.module('hummedia.services').
     factory('Account', ['$resource', 'appConfig', function($resource, config){
-        var resource = $resource(config.apiBase + '/account/:identifier', {identifier: '@identifier'},
+        var resource = $resource(config.apiBase + '/account/:_id', {_id: '@_id'},
         {
             search: {method: 'GET', isArray: true},
-            update: {method: 'PATCH'}
+            update: {method: 'PATCH', isArray: false}
         });
         return resource;
     }]);
