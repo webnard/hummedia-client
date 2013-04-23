c8c898e33dc8a7bc13d692cf774a9a1783b60c74
dcraven <dcravenus@gmail.com>
2013-04-23 15:47:38 -0600

    Navigating to no collection shows an error

--- app/js/controllers/collection.js
+++ app/js/controllers/collection.js
@@ -1,7 +1,12 @@
 'use strict';
 function CollectionCtrl($scope, $routeParams, Collection) {
-    $scope.collection_data = Collection.get({identifier:$routeParams.id});
-    console.log($scope.collection_data);
+    var pid;
+    if($routeParams.id===''){
+        pid='null';
+    }else{
+        pid=$routeParams.id;
+    }
+    $scope.collection_data = Collection.get({identifier:pid});
 }
 // always inject this in so we can later compress this JavaScript
 CollectionCtrl.$inject = ['$scope', '$routeParams', 'Collection'];
