1d856e4344cb6e82ad2cfe3a2291edeb564e989d
Ian Hunter <beanland2@gmail.com>
2013-04-22 21:49:07 -0600

    You can almost edit users now.

--- app/js/controllers/admin_user.js
+++ app/js/controllers/admin_user.js
@@ -1,5 +1,10 @@
 function AdminUserCtrl($scope, account) {
     "use strict";
+    $scope.user = null;
     $scope.users =  account.search();
+
+    $scope.setUser = function(user) {
+        $scope.user = user;
+    };
 };
 AdminUserCtrl.$inject = ['$scope','Account'];
