d140c5992dab1d4e09f2028b25f37f5ca16b2056
Ian Hunter <beanland2@gmail.com>
2013-04-30 16:45:25 -0600

    Certain pages now prompt for login if required.

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,8 @@ angular.module('hummedia', ['hummedia.config','hummedia.filters', 'hummedia.serv
     $routeProvider.when('/search', {title: "Search", templateUrl: 'partials/search/search.html', controller: SearchCtrl, reloadOnSearch: false});
     $routeProvider.when('/', {title: "Humanities Online Media", templateUrl: 'partials/home.html'});
     $routeProvider.when('/collection/:id', {title: "Collection", templateUrl: 'partials/collection.html', controller: CollectionCtrl});
-    $routeProvider.when('/admin/collection', {title: "Admin | Collections", templateUrl: 'partials/admin-collection.html', controller: AdminCollectionCtrl, reloadOnSearch: false});
-    $routeProvider.when('/admin/user', {title: "Admin | Users", templateUrl: 'partials/admin-user.html', controller: AdminUserCtrl, reloadOnSearch: false});
+    $routeProvider.when('/admin/collection', {title: "Admin | Collections", admin: true, templateUrl: 'partials/admin-collection.html', controller: AdminCollectionCtrl, reloadOnSearch: false});
+    $routeProvider.when('/admin/user', {title: "Admin | Users", admin: true, templateUrl: 'partials/admin-user.html', controller: AdminUserCtrl, reloadOnSearch: false});
     $routeProvider.when('/collection', {title: "Collections", templateUrl: 'partials/collections.html', controller: CollectionsCtrl});
     $routeProvider.when('/developer', {title: "Developer", templateUrl: 'partials/developer.html'});
     $routeProvider.otherwise({redirectTo: '/'});
@@ -37,10 +37,33 @@ angular.module('hummedia', ['hummedia.config','hummedia.filters', 'hummedia.serv
       }]);
   }]).
   /*!
-    partially adapted from jkoreska @ http://stackoverflow.com/a/13407227/390977
+    document title modification partially from jkoreska @ http://stackoverflow.com/a/13407227/390977
   */
-  run(['$route', '$rootScope', function($routeProvider, $rootScope) {
+  run(['$location', '$rootScope','user', function($location, $rootScope, user) {
+      // updates page title on URL change
       $rootScope.$on('$routeChangeSuccess', function(ev, current, previous) {
             $rootScope.title = current.$$route.title;
+      });
+
+      // require login for specific pages
+      $rootScope.$on('$routeChangeStart', function(ev, current, previous) {
+            if(!user.exists && current.$$route.login) {
+                $location.path("/");
+                setTimeout(function() {
+                    user.prompt();
+                },1); // make sure this happens AFTER the route is updated
+            }
+            else if(!user.isAdmin && current.$$route.admin) {
+                $location.path("/");
+                setTimeout(function(){
+                    if(!user.exists) {
+                        user.prompt();
+                    }
+                    else
+                    {
+                        alert("You are not authorized to view that page.");
+                    }
+                },1);
+            }
       });
   }]);
