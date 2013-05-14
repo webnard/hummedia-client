b7aa8b0a1ea16fc34a49c752281fa43513586f39
Ian Hunter <beanland2@gmail.com>
2013-05-14 13:42:48 -0600

    Title changes to make it more obvious what the site is.

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('hummedia', ['hummedia.config','hummedia.filters', 'hummedia.services', 'hummedia.directives', 'ngLocale']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/search', {title: "Search", templateUrl: 'partials/search/search.html', controller: SearchCtrl, reloadOnSearch: false});
-    $routeProvider.when('/', {title: "Humanities Online Media", templateUrl: 'partials/home.html'});
-    $routeProvider.when('/collection/:id', {title: "Collection", templateUrl: 'partials/collection.html', controller: CollectionCtrl});
-    $routeProvider.when('/admin/collection', {title: "Admin | Collections", admin: true, templateUrl: 'partials/admin-collection.html', controller: AdminCollectionCtrl, reloadOnSearch: false});
-    $routeProvider.when('/admin/user', {title: "Admin | Users", admin: true, templateUrl: 'partials/admin-user.html', controller: AdminUserCtrl, reloadOnSearch: false});
-    $routeProvider.when('/collection', {title: "Collections", templateUrl: 'partials/collections.html', controller: CollectionsCtrl});
-    $routeProvider.when('/developer', {title: "Developer", templateUrl: 'partials/developer.html'});
+    $routeProvider.when('/search', {title: "Hummedia | Search", templateUrl: 'partials/search/search.html', controller: SearchCtrl, reloadOnSearch: false});
+    $routeProvider.when('/', {title: "Hummedia", templateUrl: 'partials/home.html'});
+    $routeProvider.when('/collection/:id', {title: "Hummedia | Collection", templateUrl: 'partials/collection.html', controller: CollectionCtrl});
+    $routeProvider.when('/admin/collection', {title: "Hummedia | Collections", admin: true, templateUrl: 'partials/admin-collection.html', controller: AdminCollectionCtrl, reloadOnSearch: false});
+    $routeProvider.when('/admin/user', {title: "Hummedia | Users", admin: true, templateUrl: 'partials/admin-user.html', controller: AdminUserCtrl, reloadOnSearch: false});
+    $routeProvider.when('/collection', {title: "Hummedia | Collections", templateUrl: 'partials/collections.html', controller: CollectionsCtrl});
+    $routeProvider.when('/developer', {title: "Hummedia | Developer", templateUrl: 'partials/developer.html'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]).
   config(['$locationProvider', function($locationProvider) {
@@ -63,7 +63,6 @@ angular.module('hummedia', ['hummedia.config','hummedia.filters', 'hummedia.serv
                   return 2;
               return 0;
           }
-          
 
           if(denied()) {
               var redirect = $window.location.toString();
