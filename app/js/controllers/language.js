c107a9d6f573f7eb7b5838cc9669b12469f26658
Ian Hunter <beanland2@gmail.com>
2013-05-17 23:29:05 -0600

    Fixes problem with language label on login

--- app/js/controllers/language.js
+++ app/js/controllers/language.js
@@ -4,7 +4,7 @@
  * Allows the user to change their locale
  */
 function LanguageCtrl($scope, language) {
-    $scope.language = language.current;
+    $scope.language = language;
     $scope.languages = language.list;
     
     $scope.$watch('language', function(val) {
