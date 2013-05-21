5d1a7c2dcad6168e45ac1cb733d27cc46a250430
dcraven <dcravenus@gmail.com>
2013-05-21 15:01:55 -0600

    Player is more functional

--- app/js/controllers/video.js
+++ app/js/controllers/video.js
@@ -1,10 +1,8 @@
 'use strict';
 function VideoCtrl($scope, $routeParams, Video) {
     $scope.video = Video.get({identifier:$routeParams.id}, function(){
-        Popcorn.smart('#popcorn-player', $scope.video['url'][0]);    
+        Popcorn.smart('#popcorn-player', $scope.video['url']);    
     });
-    console.log($scope.video);
-    //Video.loadPopcorn($routeParams.id);
 }
 // always inject this in so we can later compress this JavaScript
 VideoCtrl.$inject = ['$scope', '$routeParams', 'Video'];
