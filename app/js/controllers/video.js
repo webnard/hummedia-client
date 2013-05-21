a16f3f597d55aa6d96ef413014aa20f41b87dc1e
dcraven <dcravenus@gmail.com>
2013-05-21 12:15:19 -0600

    Started branch

--- app/js/controllers/video.js
+++ app/js/controllers/video.js
@@ -1,7 +1,10 @@
 'use strict';
 function VideoCtrl($scope, $routeParams, Video) {
-    $scope.video = Video.get({identifier:$routeParams.id});
-    Video.loadPopcorn($routeParams.id);
+    $scope.video = Video.get({identifier:$routeParams.id}, function(){
+        Popcorn.smart('#popcorn-player', $scope.video['url'][0]);    
+    });
+    console.log($scope.video);
+    //Video.loadPopcorn($routeParams.id);
 }
 // always inject this in so we can later compress this JavaScript
 VideoCtrl.$inject = ['$scope', '$routeParams', 'Video'];
