b7ec3e64d790b8211d72122a969e023fdf3be2d4
Ian Hunter <beanland2@gmail.com>
2013-05-30 18:30:33 +0000

    language.js edited online with Bitbucket

--- app/js/controllers/language.js
+++ app/js/controllers/language.js
@@ -6,7 +6,8 @@
 function LanguageCtrl($scope, language) {
     $scope.language = language;
     
-    // toggles the language label's value on and off quickly
+    // toggles the language label's value on and off quickly.
+    // Otherwise Chrome won't repaint our styles correctly, and then our label is off-center
     $scope.flash = function() {
         angular.element('#nav-language-label').hide();
         setTimeout(function(){
