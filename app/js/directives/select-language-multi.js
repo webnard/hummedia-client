3186c732981f36eef472d33d1798ad0f6526a5ff
Ian Hunter <beanland2@gmail.com>
2013-05-30 10:28:18 -0600

    Everything almost working with language selection menus.

--- app/js/directives/select-language-multi.js
+++ app/js/directives/select-language-multi.js
@@ -1,22 +1,23 @@
 /**
- * Populates a language selection dropdown menu.
+ * Populates a language multiple-selection menu.
  * The value of the select-language attribute should be the ng-model value you want to bind to.
  * Defaults to language.
  * 
  * @author Ian Hunter
- * @see selectLanguageMulti and selectLanguageHelper
+ * @see selectLanguage and selectLanguageHelper
  * 
  * Attributes:
  *  lang-exists - whether or not we select our languages from languages that we have translations for
  *                do not include this attribute if you want a selection of all available languages
+ *                
  */
 "use strict";
 HUMMEDIA_DIRECTIVES.
-    directive('selectLanguage', ['selectLanguageHelper', function(SLH) {
+    directive('selectLanguageMulti', ['selectLanguageHelper', function(SLH) {
         return {
             link: SLH.linkFn,
             scope: false,
-            template: '<select ng-model="__language" data-ng-options="lang.value as lang.label | language for lang in __languages"></select>',
+            template: '<select multiple ng-model="__language" data-ng-options="lang.value as lang.label | language for lang in __languages"></select>',
             replace: true
         };
     }]);
---
+++ app/js/services/select-language-helper.js
@@ -0,0 +1,45 @@
+/**
+ * Used in the selectLanguage and selectLanguageMulti directives.
+ * Couldn't find an way to easily combine the two, as each had slightly different templates
+ * 
+ * @author Ian Hunter
+ * @see selectLanguage and selectLanguageMulti directives
+ */
+"use strict";
+HUMMEDIA_SERVICES.
+    service('selectLanguageHelper', ['language', function(language){
+        var mappings = {"ab":"аҧсуа бызшәа", "aa":"Afaraf","af":"Afrikaans","ak":"Akan","sq":"gjuha shqipe","am":"አማርኛ","ar":"العربية","an":"aragonés","hy":"Հայերեն","as":"অসমীয়া","av":"авар мацӀ","ae":"avesta","ay":"aymar aru","az":"azərbaycan dili","bm":"bamanankan","ba":"башҡорт теле","eu":"euskara","be":"беларуская мова","bn":"বাংলা","bh":"भोजपुरी","bi":"Bislama","bs":"bosanski jezik","br":"brezhoneg","bg":"български език","my":"ဗမာစာ","ca":"català","ch":"Chamoru","ce":"нохчийн мотт","ny":"chiCheŵa","zh":"中文","cv":"чӑваш чӗлхи","kw":"Kernewek","co":"corsu","cr":"ᓀᐦᐃᔭᐍᐏᐣ","hr":"hrvatski jezik","cs":"čeština","da":"dansk","dv":"ދިވެހި","nl":"Nederlands","dz":"རྫོང་ཁ","en":"English","eo":"Esperanto","et":"eesti","ee":"Eʋegbe","fo":"føroyskt","fj":"vosa Vakaviti","fi":"suomi","fr":"français","ff":"Fulfulde","gl":"galego","ka":"ქართული","de":"Deutsch","el":"ελληνικά","gn":"Avañe'ẽ","gu":"ગુજરાતી","ht":"Kreyòl ayisyen","ha":"Hausa","he":"עברית","hz":"Otjiherero","hi":"हिन्दी","ho":"Hiri Motu","hu":"magyar","ia":"Interlingua","id":"Bahasa Indonesia","ie":"Interlingue","ga":"Gaeilge","ig":"Asụsụ Igbo","ik":"Iñupiaq","io":"Ido","is":"Íslenska","it":"italiano","iu":"ᐃᓄᒃᑎᑐᑦ","ja":"日本語 (にほんご)","jv":"basa Jawa","kl":"kalaallisut","kn":"ಕನ್ನಡ","kr":"Kanuri","ks":"कश्मीरी","kk":"қазақ тілі","km":"ខ្មែរ","ki":"Gĩkũyũ","rw":"Ikinyarwanda","ky":"Кыргызча","kv":"коми кыв","kg":"KiKongo","ko":"한국어 (韓國語)","ku":"Kurdî","kj":"Kuanyama","la":"latine","lb":"Lëtzebuergesch","lg":"Luganda","li":"Limburgs","ln":"Lingála","lo":"ພາສາລາວ","lt":"lietuvių kalba","lu":"Tshiluba","lv":"latviešu valoda","gv":"Gaelg","mk":"македонски јазик","mg":"fiteny malagasy","ms":"bahasa Melayu","ml":"മലയാളം","mt":"Malti","mi":"te reo Māori","mr":"मराठी","mh":"Kajin M̧ajeļ","mn":"монгол","na":"Ekakairũ Naoero","nv":"Diné bizaad","nb":"Norsk bokmål","nd":"isiNdebele","ne":"नेपाली","ng":"Owambo","nn":"Norsk nynorsk","no":"Norsk","ii":"ꆈꌠ꒿ Nuosuhxop","nr":"isiNdebele","oc":"occitan","oj":"ᐊᓂᔑᓈᐯᒧᐎᓐ","cu":"ѩзыкъ словѣньскъ","om":"Afaan Oromoo","or":"ଓଡ଼ିଆ","os":"ирон æвзаг","pa":"ਪੰਜਾਬੀ","pi":"पाऴि","fa":"فارسی","pl":"język polski","ps":"پښتو","pt":"português","qu":"Runa Simi","rm":"rumantsch grischun","rn":"Ikirundi","ro":"limba română","ru":"русский язык","sa":"संस्कृतम्","sc":"sardu","sd":"सिन्धी","se":"Davvisámegiella","sm":"gagana fa'a Samoa","sg":"yângâ tî sängö","sr":"српски језик","gd":"Gàidhlig","sn":"chiShona","si":"සිංහල","sk":"slovenčina","sl":"slovenski jezik","so":"Soomaaliga","st":"Sesotho","es":"español","su":"Basa Sunda","sw":"Kiswahili","ss":"SiSwati","sv":"Svenska","ta":"தமிழ்","te":"తెలుగు","tg":"тоҷикӣ","th":"ไทย","ti":"ትግርኛ","bo":"བོད་ཡིག","tk":"Türkmen","tl":"Wikang Tagalog","tn":"Setswana","to":"faka Tonga","tr":"Türkçe","ts":"Xitsonga","tt":"татар теле","tw":"Twi","ty":"Reo Tahiti","ug":"Uyƣurqə","uk":"українська мова","ur":"اردو","uz":"O'zbek","ve":"Tshivenḓa","vi":"Tiếng Việt","vo":"Volapük","wa":"walon","cy":"Cymraeg","wo":"Wollof","fy":"Frysk","xh":"isiXhosa","yi":"ייִדיש","yo":"Yorùbá","za":"Saɯ cueŋƅ","zu":"isiZulu"};
+        var languages = [];
+        for(var i in mappings) {
+            if(mappings.hasOwnProperty(i)) {
+                var obj = {value: i, label: mappings[i]};
+                languages.push(obj);
+            }
+        }
+        
+        var linkFn = function(scope, iElement, iAttrs, controller) {
+            var model = iAttrs['selectLanguage'] ? iAttrs['selectLanguage'] : 'language';
+            scope.__language = scope[model];
+            
+            if(iAttrs.hasOwnProperty('langExists')) {
+                scope.__languages = language.list;
+            }
+            else
+            {
+                scope.__languages = languages;
+            }
+            
+            // these two watches keep our temporary variable and the model in sync
+            scope.$watch(model, function() {
+                scope.__language = scope.$eval(model);
+            });
+            
+            scope.$watch('__language', function() {
+                scope.$eval(model + " = __language");
+            });
+        };
+        
+        return {
+            linkFn: linkFn
+        };
+    }]);
