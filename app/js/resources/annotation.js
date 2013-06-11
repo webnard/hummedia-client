7e539612c16841abe500673d7b46f85085c8b033
Ian Hunter <beanland2@gmail.com>
2013-06-11 09:03:57 -0600

    Removing more debug code.

--- app/js/resources/annotation.js
+++ app/js/resources/annotation.js
@@ -3,7 +3,6 @@
 angular.module('hummedia.services').
     factory('Annotation', ['$resource', 'appConfig', function($resource, config){
         var resource = $resource(config.apiBase + '/annotation/:identifier', {identifier: '@pid'});
-        //annotation?dc:relation=
         
         return resource;
     }]);
