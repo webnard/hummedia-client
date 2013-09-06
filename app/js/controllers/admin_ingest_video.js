40d912ebed114a4e7a2b4365b115a67b93a7592d
Ian Hunter <beanland2@gmail.com>
2013-09-06 15:43:01 -0600

    Ingest page now passes information as needed to Jarom.

--- app/js/controllers/admin_ingest_video.js
+++ app/js/controllers/admin_ingest_video.js
@@ -3,14 +3,19 @@ function AdminIngestCtrl($scope, Video, $location) {
     $scope.creating = false;
     $scope.videos = Video.files();
 
-    $scope.setVideo = function(url) {
-        $scope.video = {url: url, 'ma:title': url};
+    $scope.setVideo = function(filepath) {
+        $scope.video = {'ma:title': filepath};
     }
 
     $scope.createVideo = function() {
+        var imdbID = $scope.imdb,
+            filepath = $scope.video['ma:title'];
+
+        $scope.video.url = [imdbID + ".mp4", imdbID + ".webm"];
+
         $scope.creating = true;
         Video.save($scope.video,function(data){
-            Video.ingest($scope.video.url, data.pid);
+            Video.ingest(filepath, data.pid, imdbID);
             $location.path("/admin/video").search({id: data.pid});
         });
     }
