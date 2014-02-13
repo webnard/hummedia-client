56bb02362132ca9d2402941dfaa6e7557047a20e
Ian Hunter <beanland2@gmail.com>
2014-02-13 14:34:45 -0700

    Maintenance message up.

--- app/js/controllers/nav.js
+++ app/js/controllers/nav.js
@@ -14,5 +14,21 @@ function NavCtrl($scope, user, config) {
 
     $scope.hasCheckedUser = false;
     user.checkStatus().then(function(){ $scope.hasCheckedUser = true; });
+
+    // Quick fix for maintenance message. Can isolate this in a method call
+    // later
+    var maintenanceDate = new Date();
+    maintenanceDate.setYear(2014);
+    maintenanceDate.setMonth(1);
+    maintenanceDate.setDate(16);
+    maintenanceDate.setHours(0);
+    maintenanceDate.setMinutes(0);
+    maintenanceDate.setSeconds(0);
+
+    if(Date.now() < maintenanceDate) {
+        $scope.maintenance = "Hummedia will be undergoing maintenance "+
+          "throughout the day on Saturday, February 15. You may experience "+
+          "interruptions of service. We apologize for the inconvenience.";
+    }
 };
 NavCtrl.$inject = ['$scope','user','appConfig'];
