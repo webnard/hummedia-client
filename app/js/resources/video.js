46ce7531c224296e0cb7e2900dd89ca57862af60
Ian Hunter <beanland2@gmail.com>
2014-04-09 12:38:26 -0600

    beginning work on enabling subtitle uploads.

--- app/js/resources/video.js
+++ app/js/resources/video.js
@@ -5,10 +5,10 @@ angular.module('hummedia.services').
         var resource = $resource(config.apiBase + '/video/:identifier', {identifier: '@identifier'},
         {
             search: {method: 'GET', isArray: true, params: {searchtype: 'keyword', q: '@q'}},
-	        advancedSearch: {method: 'GET', isArray: true},
+            advancedSearch: {method: 'GET', isArray: true},
             update: {method: 'PATCH'}
         });
-	    resource.advancedParams = ['yearfrom','yearto','ma:title','ma:description'];
+        resource.advancedParams = ['yearfrom','yearto','ma:title','ma:description'];
 
         resource.files = function() {
             var deferred = $q.defer();
@@ -20,6 +20,36 @@ angular.module('hummedia.services').
 
         resource.ingest = function(filepath, pid, uniqueID) {
             return $http.post(config.apiBase + '/batch/video/ingest',[{filepath: filepath, pid: pid, id: uniqueID}]);
+        }
+
+        /**
+         * Uploads a subtitle asynchronously to the given video.
+         * @param file Blob the file to upload
+         * @param pid String the identifier of the video
+         * @return promise Resolves when the web server returns favorably
+         */
+        resource.addSubtitle = function(file, pid) {
+            var deferred = $q.defer(),
+                formData = new FormData(),
+                request  = new XMLHttpRequest();
+
+            request.append('subtitle', file);
+            request.open('PATCH', config.apiBase + '/video/' + pid);
+            request.onload = function() {
+                if(request.status == 200) {
+                    try {
+                        deferred.resolve(JSON.parse(request.responseText));
+                    }catch(e){
+                        deferred.reject("Unable to parse response body.");
+                    }
+                }
+                else
+                {
+                    deferred.reject("Server returned status " + request.status);
+                }
+            };
+            request.send(formData);
+            return deferred;
         }
 
         return resource;
