268ca5d5c82f7755b3432d6873c8d0b4fea21cbc
Ian Hunter <beanland2@gmail.com>
2014-04-21 12:03:06 -0600

    Title sorting does a natural sort and ignores articles like "The" and "Los"

--- app/js/filters.js
+++ app/js/filters.js
@@ -16,15 +16,47 @@ angular.module('hummedia.filters', [])
 })
 // enables ordering objects by keys that have colons
 .filter('orderColon', function(){
+
+    // ignore these articles at the beginning of titles
+    var ignore = /^(The|Der|Das|Die|Le|La|El|Il|Los|Las|Les|Gli|Le)(?:\s)/i;
+
     return function(items, field, reverse) {
-      items.sort(function(a, b) {
+      if(items === undefined) {
+        return;
+      }
+      items.sort(function(item1, item2) {
+        var a = item1[field].replace(ignore,''),
+            b = item2[field].replace(ignore,'');
+        
+        var aDigits = a.match(/\d+/),
+            bDigits = b.match(/\d+/);
+
+        // Performs a natural sort with numeric titles, if applicable
+        if(aDigits && bDigits) {
+          var lastA = aDigits[aDigits.length-1],
+              lastB = bDigits[bDigits.length-1];
+
+          var aIdx = a.lastIndexOf(lastA),
+              bIdx = b.lastIndexOf(lastB);
+
+          var aSansDigits = a.slice(0, aIdx) + a.slice(aIdx + lastA.length),
+              bSansDigits = b.slice(0, bIdx) + b.slice(bIdx + lastB.length);
+
+          if(aSansDigits === bSansDigits) {
+            var intA = Number.parseInt(lastA),
+                intB = Number.parseInt(lastB);
+
+            return intA > intB ? 1 : intA < intB ? -1 : 0;
+          }
+        }
+
         // localeCompare is sloooowwww, so don't use it.
         if(!reverse) {
-          return (a[field] > b[field]) ? 1 : ((a[field] < b[field]) ? -1 : 0);
+          return (a > b) ? 1 : ((a < b) ? -1 : 0);
         }
         else
         {
-          return (a[field] < b[field]) ? 1 : ((a[field] > b[field]) ? -1 : 0);
+          return (a < b) ? 1 : ((a > b) ? -1 : 0);
         }
       });
       return items;
